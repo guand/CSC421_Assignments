a5e10e5c38643e994d3bd098045f5463aea3cecb
guand <guan3d@gmail.com>
2015-09-27 17:15:22 -0700

    updated map generation and completed dfs and bfs

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,8 @@
 import random
 import numpy
 import heapq
-
+import string
+import itertools
 ## 
 # Generates random position for the 25 cities
 ##
@@ -25,6 +26,8 @@ def euclideanMapModify(cityList):
 			a = numpy.array(first)
 			b = numpy.array(second)
 			distance = numpy.linalg.norm(a - b)
+			if(distance == 0.0):
+				distance = 100000.0
 			euclideanDistanceCities.append(distance)
 		euclideanMap.append(euclideanDistanceCities)
 	return euclideanMap
@@ -34,22 +37,78 @@ def euclideanMapModify(cityList):
 # remove 2 random lowest euclidean distances 
 ##
 def pruneEuclideanMap(euclideanList):
+	euclideanFinalPruneList = []
 	for i, first in enumerate(euclideanList):
 		euclideanPruneList = []
-		for j in heapq.nsmallest(6, enumerate(euclideanList[i]), key=lambda x:x[1]):
-			if (100000 >= j[1] > 0):
-				euclideanPruneList.append(j[0])
-		cityPrune = random.sample(range(0, len(euclideanPruneList)), 2)
+		for j in heapq.nsmallest(5, enumerate(euclideanList[i]), key=lambda x:x[1]):
+			euclideanPruneList.append(j)
+		cityPrune = random.sample(range(0, len(euclideanPruneList)), 3)
+		cityEuclideanList = []
 		for k in cityPrune:
-			euclideanList[i][euclideanPruneList[k]] = 100000.0
-			euclideanList[euclideanPruneList[k]][i] = 100000.0
-	for i, first in enumerate(euclideanList):
-		for j, second in enumerate(euclideanList[i]):
-			if(euclideanList[i][j] == 100000.0):
-				euclideanList[i][j] = 0.0
-	return euclideanList
+			cityEuclideanList.append(euclideanPruneList[k])
+		euclideanFinalPruneList.append(cityEuclideanList)
+# make adjacency matrix so that we can remove undirected edges
+	euclideanFinalMatrix = [[0] * len(euclideanFinalPruneList) for i in range(len(euclideanFinalPruneList))]
+	for i in range(len(euclideanFinalPruneList)):
+		for j in range(len(euclideanFinalPruneList[i])):
+			euclideanFinalMatrix[i][euclideanFinalPruneList[i][j][0]] = euclideanFinalPruneList[i][j][1]
+	for i in range(len(euclideanFinalMatrix)):
+		for j in range(len(euclideanFinalMatrix[i])):
+			if((euclideanFinalMatrix[i][j] != euclideanFinalMatrix[j][i]) and euclideanFinalMatrix[i][j] != 0):
+				euclideanFinalMatrix[j][i] = euclideanFinalMatrix[i][j];
+	euclideanFinalTupleList = []
+	for i, value in enumerate(euclideanFinalMatrix):
+		euclideanTuples = []
+		for j, tupleValue in enumerate(euclideanFinalMatrix[i]):
+			if(tupleValue != 0):
+				euclideanTuples.append((j, tupleValue))
+		euclideanFinalTupleList.append(euclideanTuples)
+	return euclideanFinalTupleList
+
+def createUnweightedGrapth(graph):
+	d = dict.fromkeys(string.ascii_uppercase, 0)
+	for i, a in enumerate(graph):
+		listSet = []
+		for j, b in enumerate(graph[i]):
+			listSet.append(chr(b[0] + ord('A')))
+		d[chr(i + ord('A'))] = set(listSet)
+	return d
+
+def dfs(graph, start, goal):
+	stack = [(start, [start])]
+	while stack:
+		(vertex, path) = stack.pop()
+		for next in graph[vertex] - set(path):
+			if next == goal:
+				yield path + [next]
+			else:
+				stack.append((next, path + [next]))
+
+def bfs(graph, start, goal):
+	queue = [(start, [start])]
+	while queue:
+		(vertex, path) = queue.pop(0)
+		for next in graph[vertex] - set(path):
+			if next == goal:
+				yield path + [next]
+			else:
+				queue.append((next, path + [next]))
+
+def dfs_for_id(route, depth):
+	return 0
+
+def id_dfs(puzzle, goal, get_moves):
+	return 0
+
+def main():
+	mapValue = randomMapGeneration()
+	euclideanMap = euclideanMapModify(mapValue)
+	weightedGraph = pruneEuclideanMap(euclideanMap)
+	unWeightedGraph = createUnweightedGrapth(weightedGraph)
 
 
-mapValue = randomMapGeneration()
-euclideanMap = euclideanMapModify(mapValue)
-print(pruneEuclideanMap(euclideanMap))
+	dfs(unWeightedGraph, 'A', 'B')
+	bfs(unWeightedGraph, 'A', 'B')
+
+if __name__ == '__main__':
+	main()
