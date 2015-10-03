26dab34b53eeea2e1ab525c742fcd8586d10bdd9
guand <guan3d@gmail.com>
2015-10-03 02:12:40 -0700

    Added Benchmarking

--- assignment1.py
+++ assignment1.py
@@ -9,7 +9,15 @@ import traverse
 from graph import Graph
 
 def main():
-	for i in range(100):
+# Heuristic Bench marks
+	misses = 0
+ 	incrementor = 0
+ 	aStarEuclideanList = []
+ 	aStarChebyshevList = []
+ 	greedyEuclideanList = []
+ 	greedyChebyshevList = []
+ 	startTime = time.time();
+	while incrementor < 100:
 		startPoint = 0
 		endPoint = 0
 		while startPoint == endPoint:
@@ -25,82 +33,161 @@ def main():
 		graph.edges = unWeightedGraph
 		graph.weights = weightedGraphMapping
 
-		startTime = time.time();
 		previous, currentCost, aStarTimeComplexity, aStarSpaceComplexity = heuristics.aStarSearch(graph, startPoint, endPoint, 1)
-		totalTime = time.time() - startTime;
-		print "Astar with Euclidean Distance"
-		print heuristics.reconstructPath(previous, startPoint, endPoint)
-		print aStarTimeComplexity
-		print aStarSpaceComplexity
-		print totalTime
-		startTime = time.time();
-		previous, currentCost, aStarTimeComplexity, aStarSpaceComplexity = heuristics.aStarSearch(graph, startPoint, endPoint, 2)
-		totalTime = time.time() - startTime;
-		print "Astar with Chebyshev Distance"
-		print heuristics.reconstructPath(previous, startPoint, endPoint)
-		print aStarTimeComplexity
-		print aStarSpaceComplexity
-		print totalTime
-		startTime = time.time();
+		path = heuristics.reconstructPath(previous, startPoint, endPoint)
+		if path == None:
+			misses = misses + 1 
+			continue
+		aStarEuclideanList.append([path, aStarTimeComplexity, aStarSpaceComplexity])
+		path = heuristics.reconstructPath(previous, startPoint, endPoint)
+		aStarChebyshevList.append([path, aStarTimeComplexity, aStarSpaceComplexity])
 		previous, greedyTimeComplexity, greedySpaceComplexity = heuristics.greedyFirstSearch(graph, startPoint, endPoint, 1)
-		totalTime = time.time() - startTime;
-		print "Greedy with Euclidean Distance"
-		print heuristics.reconstructPath(previous, startPoint, endPoint)
-		print greedyTimeComplexity
-		print greedySpaceComplexity
-		print totalTime
-		startTime = time.time();
+		path = heuristics.reconstructPath(previous, startPoint, endPoint)
+		greedyEuclideanList.append([path, greedyTimeComplexity, greedySpaceComplexity])
 		previous, greedyTimeComplexity, greedySpaceComplexity = heuristics.greedyFirstSearch(graph, startPoint, endPoint, 2)
-		totalTime = time.time() - startTime;
-		print "Greedy with Chebyshev Distance"
-		print heuristics.reconstructPath(previous, startPoint, endPoint)
-		print greedyTimeComplexity
-		print greedySpaceComplexity
-		print totalTime
+		path = heuristics.reconstructPath(previous, startPoint, endPoint)
+		greedyChebyshevList.append([path, greedyTimeComplexity, greedySpaceComplexity])
+		incrementor = incrementor + 1
+	totalTime = time.time() - startTime;
+	print "Total Execution Time"
+	print totalTime
+	print "Total Number of Invalid Paths"
+	print misses
+	averageTime = 0
+	averageSpace = 0
+	for i in aStarEuclideanList:
+		averageTime = averageTime + i[1]
+		averageSpace = averageSpace + i[2]
+	print "Average A* Euclidean Time Complexity"
+	print averageTime / len(aStarEuclideanList)
+	print "Average A* Euclidean Space Complexity"
+	print averageSpace / len(aStarEuclideanList)
+	averageTime = 0
+	averageSpace = 0
+	for i in aStarChebyshevList:
+		averageTime = averageTime + i[1]
+		averageSpace = averageSpace + i[2]
+	print "Average A* Chebyshev Time Complexity"
+	print averageTime / len(aStarChebyshevList)
+	print "Average A* Chebyshev Space Complexity"
+	print averageSpace / len(aStarChebyshevList)
+	averageTime = 0
+	averageSpace = 0
+	for i in greedyEuclideanList:
+		averageTime = averageTime + i[1]
+		averageSpace = averageSpace + i[2]
+	print "Average Greedy Euclidean Time Complexity"
+	print averageTime / len(greedyEuclideanList)
+	print "Average Greedy Euclidean Space Complexity"
+	print averageSpace / len(greedyEuclideanList)
+	averageTime = 0
+	averageSpace = 0
+	for i in greedyChebyshevList:
+		averageTime = averageTime + i[1]
+		averageSpace = averageSpace + i[2]
+	print "Average Greedy Chebyshev Time Complexity"
+	print averageTime / len(greedyChebyshevList)
+	print "Average Greedy Chebyshev Space Complexity"
+	print averageSpace / len(greedyChebyshevList)
+# Get 10 test results of each heuristic
+	print "A* Euclidean Heuristic"
+	for i in aStarEuclideanList[:10]:
+		print "Path: ", i[0]
+		print "Time Complexity: ", i[1]
+		print "Space Complexity: ", i[2]
+	print "A* Chebyshev Heuristic"
+	for i in aStarChebyshevList[:10]:
+		print "Path: ", i[0]
+		print "Time Complexity: ", i[1]
+		print "Space Complexity: ", i[2]
+	print "Greedy Euclidean Heuristic"
+	for i in greedyEuclideanList[:10]:
+		print "Path: ", i[0]
+		print "Time Complexity: ", i[1]
+		print "Space Complexity: ", i[2]
+	print "Greedy Chebyshev Heuristic"
+	for i in greedyChebyshevList[:10]:
+		print "Path: ", i[0]
+		print "Time Complexity: ", i[1]
+		print "Space Complexity: ", i[2]
 
+# BFS, DFS, IDDFS benchmarks
+ 	breadthFirstSearchList = []
+ 	depthFirstSearchList = []
+ 	iterativeDeepeningList = []
+ 	misses = 0
+ 	incrementor = 0
+ 	startTime = time.time();
+ 	while incrementor < 100:
+		startPoint = 0
+		endPoint = 0
+		while startPoint == endPoint:
+			startPoint = chr(random.randint(0, 25) + ord('A')) 
+			endPoint = chr(random.randint(0, 25) + ord('A'))
+					
+		mapValue = graphgeneration.randomMapGeneration()
+		euclideanMap = graphgeneration.euclideanMapModify(mapValue)
+		weightedGraph = graphgeneration.pruneEuclideanMap(euclideanMap)
+		unWeightedGraph = graphgeneration.createUnweightedSetGrapth(weightedGraph)
+		breadthFirstSearch, breadthFirstTimeComplexity, breadthFirstSearchSpaceComplexity = traverse.bfs(unWeightedGraph, startPoint, endPoint)
+		if breadthFirstSearch == None:
+			misses = misses + 1 
+			continue
+		breadthFirstSearchList.append([breadthFirstSearch, breadthFirstTimeComplexity, breadthFirstSearchSpaceComplexity])
+		depthFirstSearch, depthFirstSearchTimeComplexity, depthFirstSearchSpaceComplexity = traverse.dfs(unWeightedGraph, startPoint, endPoint)
+		depthFirstSearchList.append([depthFirstSearch, depthFirstSearchTimeComplexity, depthFirstSearchSpaceComplexity])
+		iterativeDeeping, iterativeDeepingTimeComplexity, iterativeDeepingSpaceComplexity = traverse.id_dfs(unWeightedGraph, startPoint, endPoint)
+		iterativeDeepeningList.append([iterativeDeeping, iterativeDeepingTimeComplexity, iterativeDeepingSpaceComplexity])
+		incrementor = incrementor + 1
+	totalTime = time.time() - startTime;
+	print "Total Execution Time"
+	print totalTime
+	print "Total Number of Invalid Paths"
+	print misses
+	averageTimeBFS = 0
+	averageSpaceBFS = 0
+	for breadth in breadthFirstSearchList:
+		averageTimeBFS = averageTimeBFS + breadth[1]
+		averageSpaceBFS = averageSpaceBFS + breadth[2]
+	print "Average BFS Time Complexity"
+	print averageTimeBFS / len(breadthFirstSearchList)
+	print "Average BFS Space Complexity"
+	print averageSpaceBFS / len(breadthFirstSearchList)
+	averageTimeDFS = 0
+	averageSpaceDFS = 0
+	for depth in depthFirstSearchList:
+		averageTimeDFS = averageTimeDFS + depth[1]
+		averageSpaceDFS = averageSpaceDFS + depth[2]
+	print "Average DFS Time Complexity"
+	print averageTimeDFS / len(depthFirstSearchList)
+	print "Average DFS Space Complexity"
+	print averageSpaceDFS / len(depthFirstSearchList)
+	averageTimeIDDFS = 0
+	averageSpaceIDDFS = 0
+	for iddfs in iterativeDeepeningList:
+		averageTimeIDDFS = averageTimeIDDFS + iddfs[1]
+		averageSpaceIDDFS = averageSpaceIDDFS + iddfs[2]
+	print "Average IDDFS Time Complexity"
+	print averageTimeIDDFS / len(iterativeDeepeningList)
+	print "Average IDDFS Space Complexity"
+	print averageSpaceIDDFS / len(iterativeDeepeningList)
+	# Get 10 results of each benchmark
+	print "Breadth First Search"
+	for breadth in breadthFirstSearchList[:10]:
+		print "Path: ", breadth[0]
+		print "Time Complexity: ", breadth[1]
+		print "Space Complexity: ", breadth[2]
+	print "Depth First Search"
+	for depth in depthFirstSearchList[:10]:
+		print "Path: ", depth[0]
+		print "Time Complexity: ", depth[1]
+		print "Space Complexity: ", depth[2]
+	print "Iterative Deepening First Search"
+	for iddfs in iterativeDeepeningList[:10]:
+		print "Path: ", iddfs[0]
+		print "Time Complexity: ", iddfs[1]
+		print "Space Complexity: ", iddfs[2]
 
-	# graph = {'A': set(['B', 'C']),
- #         'B': set(['A', 'D', 'E']),
- #         'C': set(['A', 'F']),
- #         'D': set(['B']),
- #         'E': set(['B', 'F']),
- #         'F': set(['C', 'E'])}
- 	
- 	# for i in range(100):
-	# startPoint = 0
-	# endPoint = 0
-	# while startPoint == endPoint:
-	# 	startPoint = chr(random.randint(0, 25) + ord('A')) 
-	# 	endPoint = chr(random.randint(0, 25) + ord('A'))
-				
-	# mapValue = graphgeneration.randomMapGeneration()
-	# euclideanMap = graphgeneration.euclideanMapModify(mapValue)
-	# weightedGraph = graphgeneration.pruneEuclideanMap(euclideanMap)
-	# unWeightedGraph = graphgeneration.createUnweightedSetGrapth(weightedGraph)
-	# startTime = time.time();
-	# breadthFirstSearch, breadthFirstTimeComplexity, breadthFirstSearchSpaceComplexity = traverse.bfs(unWeightedGraph, startPoint, endPoint)
-	# totalTime = time.time() - startTime;
-	# print "Breadth First Search"
-	# print breadthFirstSearch
-	# print breadthFirstTimeComplexity
-	# print breadthFirstSearchSpaceComplexity
-	# print totalTime
-	# startTime = time.time();
-	# depthFirstSearch, depthFirstSearchTimeComplexity, depthFirstSearchSpaceComplexity = traverse.dfs(unWeightedGraph, startPoint, endPoint)
-	# totalTime = time.time() - startTime;
-	# print "Depth First Search"
-	# print depthFirstSearch
-	# print depthFirstSearchTimeComplexity
-	# print depthFirstSearchSpaceComplexity
-	# print totalTime
-	# startTime = time.time();
-	# iterativeDeeping, iterativeDeepingTimeComplexity, iterativeDeepingSpaceComplexity = traverse.id_dfs(unWeightedGraph, startPoint, endPoint)
-	# totalTime = time.time() - startTime;
-	# print "Iterative Deeping"
-	# print iterativeDeeping
-	# print iterativeDeepingTimeComplexity
-	# print iterativeDeepingSpaceComplexity
-	# print totalTime
 
 
 if __name__ == '__main__':
