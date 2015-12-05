ee623f94b337a5ebe6a03c4f81a5413a187f6381
guand <guan3d@gmail.com>
2015-12-05 07:42:23 -0800

    Added assignment 3

---
+++ CSC421_Assignment_3/assignment3_part1.py
@@ -0,0 +1,14 @@
+
+def replace_implications(implication):
+	nex_expression = ''
+	for i in implication:
+
+
+
+def main():
+	operators = ['|', '&', '!', '~', '>']
+	propositions = ['a', 'b', 'c', 'd', 'e', 'f']
+
+
+if __name__ == "__main__":
+    main()
